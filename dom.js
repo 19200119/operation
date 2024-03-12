d07b6c819160ba31c9c817c7d3d660bbcd28ed27
Nandini gowda <151017193+19200119@users.noreply.github.com>
2024-03-12 13:41:33 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,8 @@
+// let h1 = document.createElement("h1")
+// h1.innertext = "hellooo"
+// document.body.appendChild(h1)
+
+// let p = document.createElement
+// p.innertext = "web is the best"
+// document.body.appendChild(p)
+
