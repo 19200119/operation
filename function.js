f2adb5f4a288235d2012e48d67b38a1034c6bb8f
Nandini gowda <151017193+19200119@users.noreply.github.com>
2024-03-12 13:45:08 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,13 @@
+// function login()
+// {
+//     console.log("hellooo")
+// }
+// login()
+
+let username = "sam"
+let obj = {
+    username : "shashi" ,
+    console.log.log(this)
+}
+}
+obj.address()
