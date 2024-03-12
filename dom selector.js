80de8fc28ba719ad3b41ac2e2b63066685a70b17
Nandini gowda <151017193+19200119@users.noreply.github.com>
2024-03-12 13:40:39 +0530

    Add files via upload

---
+++ dom selector.js
@@ -0,0 +1,18 @@
+// let h2 = document.getElementById(heading-2)
+// console.log(h2);
+
+
+//document.getElemntByclassName
+
+// let h1 = document.getElementsByClassName("heading1")
+// h1[1].style.color="red"
+// h1[2].style.color="violet"
+
+//document.getelementByTagname
+
+// let h1 = document.getElementsByTagName('h1')
+// console.log(h1[1]);
+
+// let h1 = document.getElementsByClassName("heading1")
+// h1[1].style.color="red"
+// h1[2].style.color="violet"
